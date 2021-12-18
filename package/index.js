d95ca958b0ab12a40fbb6912889f1eb5a50050a5
Shaz Momin <shaz.momin@utexas.edu>
2021-12-18 12:52:04 -0600

    initial commit

---
+++ package/index.js
@@ -0,0 +1,6 @@
+// Import necessary dependencies -> stock APIs
+// Core function that calculates gains/loses from stocks using DCA strategy
+function analyze_dca() {
+}
+module.exports = analyze_dca;
+console.log("Hello World!");
---
+++ package/index.ts
@@ -0,0 +1,11 @@
+// Import necessary dependencies -> stock APIs
+var yahooFinance = require('yahoo-finance');
+
+// Core function that calculates gains/loses from stocks using DCA strategy
+function analyze_dca() {
+    
+}
+
+module.exports = analyze_dca;
+
+console.log("Hello World!");
